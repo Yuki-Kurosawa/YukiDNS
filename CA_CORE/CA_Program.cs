c4ff981194a5c435a6030d4003fad3b1be410b86
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2023-11-22 20:57:05 +0800

    Add Some Command Lines for CA

--- CA_CORE/CA_Program.cs
+++ CA_CORE/CA_Program.cs
@@ -23,6 +23,125 @@ namespace YukiDNS.CA_CORE
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Yuki Certificate Authority 0.1.4");
+            Console.WriteLine("1. Generate Self Signed Certificate");
+            Console.WriteLine("2. Exit");
+            Console.Write("Please Input Your Choice: ");
+            string input=Console.ReadLine();
+            switch(input)
+            {
+                case "1": GenerateSelfSign();break;
+                case "2": return;
+            }
+        }
+
+        public static void GenerateSelfSign()
+        {
+            Console.Clear();
+            Console.WriteLine("Generate Self Signed Certificate");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Please Input Your Name in X509 Format:");
+
+            string name=Console.ReadLine();
+
+            #region DO ROOT CA
+            var keyr = new RSACryptoServiceProvider(2048);
+            var key = DotNetUtilities.GetRsaKeyPair(keyr);
+            Asn1SignatureFactory asn = new Asn1SignatureFactory("SHA256withRSA", key.Private, new SecureRandom());
+
+
+            var gen = new X509V3CertificateGenerator();
+            gen.SetIssuerDN(new X509Name(name));
+            gen.SetSubjectDN(new X509Name(name));
+            gen.SetSerialNumber(new BigInteger("1"));
+            gen.SetNotBefore(DateTime.Now.Date);
+            gen.SetNotAfter(DateTime.Now.AddDays(1).Date);
+            gen.SetPublicKey(key.Public);
+
+            // extended information
+            gen.AddExtension(X509Extensions.AuthorityKeyIdentifier, false, new AuthorityKeyIdentifierStructure(key.Public));
+            gen.AddExtension(X509Extensions.SubjectKeyIdentifier, false, new SubjectKeyIdentifierStructure(key.Public));
+
+            gen.AddExtension(X509Extensions.ExtendedKeyUsage, false, new ExtendedKeyUsage(
+                new DerObjectIdentifier[] {
+                    KeyPurposeID.id_kp_serverAuth,
+                    KeyPurposeID.id_kp_clientAuth,
+                    new DerObjectIdentifier("1.2.3.4.5.6")
+                }));
+            gen.AddExtension(X509Extensions.BasicConstraints, true, new BasicConstraints(3));
+            gen.AddExtension(X509Extensions.KeyUsage, true, new KeyUsage(KeyUsage.DigitalSignature | KeyUsage.KeyEncipherment | KeyUsage.CrlSign | KeyUsage.KeyCertSign));
+
+            GeneralNames gns1 = new GeneralNames(new GeneralName[] {
+                    new GeneralName(GeneralName.DnsName,"localhost"),
+                    new GeneralName(GeneralName.IPAddress,"127.0.0.1"),
+                    new GeneralName(GeneralName.IPAddress,"::1"),
+                    new GeneralName(GeneralName.Rfc822Name,"admin@test.root"),
+                    new GeneralName(GeneralName.Rfc822Name,"www@test.root"),
+                });
+
+            gen.AddExtension(X509Extensions.SubjectAlternativeName, false, gns1.ToAsn1Object());
+
+            AuthorityInformationAccess aia = new AuthorityInformationAccess(new[] { 
+                new AccessDescription(X509ObjectIdentifiers.OcspAccessMethod, new GeneralName(GeneralName.UniformResourceIdentifier, "http://localhost/ocsp")) ,
+                new AccessDescription(X509ObjectIdentifiers.IdADCAIssuers, new GeneralName(GeneralName.UniformResourceIdentifier, "http://localhost/ca.cer")) ,
+            });
+
+            gen.AddExtension(X509Extensions.AuthorityInfoAccess, false, aia.ToAsn1Object());
+
+            var cert = gen.Generate(asn);
+
+            StringBuilder pb = new StringBuilder();
+            PemWriter pw = new PemWriter(new StringWriter(pb));
+            pw.WriteObject(cert);
+            string ca = pb.ToString();
+            File.WriteAllText("A:\\nginx\\conf\\cert.cer", ca);
+
+            StringBuilder pb2 = new StringBuilder();
+            PemWriter pw2 = new PemWriter(new StringWriter(pb2));
+            pw2.WriteObject(key.Private);
+            string ca2 = pb2.ToString();
+            File.WriteAllText("A:\\nginx\\conf\\cert.pem", ca2);
+
+            var certEntry = new X509CertificateEntry(cert);
+            var store = new Pkcs12StoreBuilder().Build();
+            store.SetCertificateEntry("CERT", certEntry);   //设置证书  
+            var chain = new X509CertificateEntry[1];
+            chain[0] = certEntry;
+            store.SetKeyEntry("CERT", new AsymmetricKeyEntry(key.Private), chain);   //设置私钥  
+            SecureRandom random = new SecureRandom();
+            using (var fs = File.Create("1.ca.pfx"))
+            {
+                store.Save(fs, "".ToCharArray(), random); //保存  
+            };
+
+            X509ExtensionsGenerator sg = new X509ExtensionsGenerator();
+            GeneralNames gns = new GeneralNames(new GeneralName[] {
+                    new GeneralName(GeneralName.DnsName,"www.test.root"),
+                    new GeneralName(GeneralName.DnsName,"test.root"),
+                    new GeneralName(GeneralName.IPAddress,"127.0.0.1"),
+                    new GeneralName(GeneralName.IPAddress,"::1"),
+                    new GeneralName(GeneralName.Rfc822Name,"admin@test.root"),
+                    new GeneralName(GeneralName.Rfc822Name,"www@test.root"),
+                });
+            sg.AddExtension(X509Extensions.SubjectAlternativeName, false, gns.ToAsn1Object());
+            X509Extensions sans = sg.Generate();
+            Asn1Set asn1 = new DerSet(new AttributeX509(PkcsObjectIdentifiers.Pkcs9AtExtensionRequest, new DerSet(sans)));
+
+            Pkcs10CertificationRequest request = new Pkcs10CertificationRequest("sha256withRSA", new X509Name("CN=TEST ROOT CA"), key.Public, asn1, key.Private);
+            StringBuilder pb3 = new StringBuilder();
+            PemWriter pw3 = new PemWriter(new StringWriter(pb3));
+            pw3.WriteObject(request);
+            string ca3 = pb3.ToString();
+            File.WriteAllText("1.ca.csr", ca3);
+
+            
+
+            #endregion
+
+        }
+
+        public static void Main1(string[] args)
+        {
             #region DO ROOT CA
             var keyr = new RSACryptoServiceProvider(2048);
             var key = DotNetUtilities.GetRsaKeyPair(keyr);
