51ff1965251250e1db998710444202f6e94cd963
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2023-12-12 18:02:36 +0800

    Fix CNAME via A/AAAA Bugs

--- DNS_CORE/DNSService.cs
+++ DNS_CORE/DNSService.cs
@@ -243,9 +243,11 @@ namespace YukiDNS.DNS_CORE
                             var nq = dret.RRQueries[0].ChangeQueryType(QTYPES.CNAME,selected.Name);
                             answers = BuildResponse(nq, zds);
                             var cname = zds[0].Data[0].ToString();
-                            var dnsq = dns.Copy();
-                            dnsq.RRQueries[0] = dns.RRQueries[0].ChangeName(cname);
-                            answers.AddRange(Resolve(dnsq).RRAnswer);
+                            var dnsq = new DNSRequest();
+                            dnsq.RRQueries = new List<RRQuery>(1).ToArray();
+                            dnsq.Query = 1;
+                            dnsq.RRQueries[0] = dret.RRQueries[0].ChangeName(cname);
+                            //answers.AddRange(Resolve(dnsq).RRAnswer);
                         }
                         else
                         {
