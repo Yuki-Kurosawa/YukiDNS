d7570d57b479267a1a095c8863a99da998719a12
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2024-02-02 21:15:44 +0800

    Add more ACME Methods

--- ACME_CORE/ACMEService.cs
+++ ACME_CORE/ACMEService.cs
@@ -24,7 +24,33 @@ namespace YukiDNS.ACME_CORE
 
             Console.WriteLine(retDic);
 
-            var objDic=JsonConvert.DeserializeObject<JObject>(retDic);
+            var objDic = JsonConvert.DeserializeObject<JObject>(retDic);
+
+            string nonce = GetNewNonce(httpClient, objDic);
+
+
+            RSAParameters acmeKey = RSACryptoHelper.CreateNewKey();
+
+            Console.WriteLine("Creating New Account ......");
+
+            //New Account            
+            string kid = NewAccount(httpClient, objDic, nonce, new[] {
+                "mailto:admin@test.com"
+            },acmeKey  );
+
+            nonce = GetNewNonce(httpClient, objDic);
+
+            Console.WriteLine("Creating New Order ......");
+            //New Order
+            NewOrder(httpClient, objDic, nonce, kid, new[] {
+                "test.ksyuki.com",
+                "test2.ksyuki.com"
+            },acmeKey);
+
+        }
+
+        public static string GetNewNonce(HttpClient httpClient, JObject objDic)
+        {
 
             HttpRequestMessage newNonceReq = new HttpRequestMessage();
             newNonceReq.Method = HttpMethod.Head;
@@ -38,35 +64,33 @@ namespace YukiDNS.ACME_CORE
 
             string nonce = hNonce[0];
 
-            RSAParameters acmeKey = RSACryptoHelper.CreateNewKey();
-           
+            return nonce;
+        }
 
-            Dictionary<string,string> dicNewAcct = new Dictionary<string,string>();
-            dicNewAcct["protected"] = Base64Tool.UrlEncodeFromString(JsonConvert.SerializeObject(new 
+        public static string NewAccount(HttpClient httpClient, JObject objDic,string nonce, string[] contact, RSAParameters acmeKey)
+        {
+            Dictionary<string, string> dicNewAcct = new Dictionary<string, string>();
+            dicNewAcct["protected"] = Base64Tool.UrlEncodeFromString(JsonConvert.SerializeObject(new
             {
-                alg="RS256",
-                nonce=nonce,
-                url= objDic.GetValue("newAccount").ToString(),
-                jwk=new
+                alg = "RS256",
+                nonce = nonce,
+                url = objDic.GetValue("newAccount").ToString(),
+                jwk = new
                 {
-                    e=Base64Tool.UrlEncode(acmeKey.Exponent),
-                    kty="RSA",
-                    n=Base64Tool.UrlEncode(acmeKey.Modulus)
+                    e = Base64Tool.UrlEncode(acmeKey.Exponent),
+                    kty = "RSA",
+                    n = Base64Tool.UrlEncode(acmeKey.Modulus)
                 }
             }));
             dicNewAcct["payload"] = Base64Tool.UrlEncodeFromString(JsonConvert.SerializeObject(new
             {
-                termsOfServiceAgreed=true,
-                contact = new[]
-                {
-                    "mailto:admin@test.com"
-                }
+                termsOfServiceAgreed = true,
+                contact = contact
             }));
 
-            var sign = RSACryptoHelper.Sign(acmeKey, Encoding.UTF8.GetBytes($@"{dicNewAcct["protected"]}.{dicNewAcct["payload"]}"), "SHA256");
+            var signAccount = RSACryptoHelper.Sign(acmeKey, Encoding.UTF8.GetBytes($@"{dicNewAcct["protected"]}.{dicNewAcct["payload"]}"), "SHA256");
 
-            dicNewAcct["signature"] = Base64Tool.UrlEncode(sign);
-
+            dicNewAcct["signature"] = Base64Tool.UrlEncode(signAccount);
 
             HttpContent newAcctReq = new StringContent(JsonConvert.SerializeObject(dicNewAcct));
 
@@ -75,6 +99,63 @@ namespace YukiDNS.ACME_CORE
             var newAcctResp = httpClient.PostAsync(objDic.GetValue("newAccount").ToString(), newAcctReq).Result;
 
             Console.WriteLine(newAcctResp.Content.ReadAsStringAsync().Result);
+
+            string kid = newAcctResp.Headers.GetValues("Location").ToList()[0];
+
+            Console.WriteLine(kid);
+
+            return kid;
         }
+    
+        public static string NewOrder(HttpClient httpClient, JObject objDic,string nonce,string kid, string[] dnsNames, RSAParameters acmeKey)
+        {
+            Dictionary<string, string> dicNewOrder = new Dictionary<string, string>();
+            dicNewOrder["protected"] = Base64Tool.UrlEncodeFromString(JsonConvert.SerializeObject(new
+            {
+                alg = "RS256",
+                nonce = nonce,
+                url = objDic.GetValue("newOrder").ToString(),
+                kid = kid
+            }));
+
+            List<dynamic> identifiers = new List<dynamic>();
+            foreach(var dnsName in dnsNames)
+            {
+                identifiers.Add(new
+                {
+                    type = "dns",
+                    value = dnsName,
+                });
+            }
+
+            dicNewOrder["payload"] = Base64Tool.UrlEncodeFromString(JsonConvert.SerializeObject(new
+            {
+                identifiers = identifiers.ToArray()
+            }));
+
+            var signOrder = RSACryptoHelper.Sign(acmeKey, Encoding.UTF8.GetBytes($@"{dicNewOrder["protected"]}.{dicNewOrder["payload"]}"), "SHA256");
+
+            dicNewOrder["signature"] = Base64Tool.UrlEncode(signOrder);
+            HttpContent newOrderReq = new StringContent(JsonConvert.SerializeObject(dicNewOrder));
+
+            newOrderReq.Headers.ContentType = new MediaTypeHeaderValue("application/jose+json");
+
+            var newOrderResp = httpClient.PostAsync(objDic.GetValue("newOrder").ToString(), newOrderReq).Result;
+
+            string retNewOrder = newOrderResp.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(retNewOrder);
+
+            JObject jOrder = JsonConvert.DeserializeObject<JObject>(retNewOrder);
+
+            string[] authorizations = JsonConvert.DeserializeObject<string[]>(JsonConvert.SerializeObject(jOrder.GetValue("authorizations")));
+            string oid = newOrderResp.Headers.GetValues("Location").ToList()[0];
+
+            Console.WriteLine(jOrder.GetValue("finalize").ToString());
+            Console.WriteLine(JsonConvert.SerializeObject(authorizations));
+
+            return oid;
+        }
+
     }
 }
